cc43e84600fb99aa955246fa48d34a9aec623e20
Mina Mikhael <mina.s.mikhael@gmail.com>
2022-08-12 03:10:19 +0200

    added process.env.PORT for heroku

--- api/server.js
+++ api/server.js
@@ -1,5 +1,14 @@
 const express = require("express");
 
 const server = express();
+server.use(express.json());
+
+server.post("/", (req, res) => {
+  res.send(req.body);
+});
+
+server.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./api/server");
 
-const PORT = 9000;
+const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
   console.log("server listening on PORT: ", PORT);
